f89cb6e203564a10100a6b875919bd823f510a54
Vad1m198 <Persidskij_on_ferrari@ukr.net>
2017-03-10 15:26:00 +0200

    security changes

--- src/main/java/org/dynamo/applicantsapp/config/WebSecurityConfig.java
+++ src/main/java/org/dynamo/applicantsapp/config/WebSecurityConfig.java
@@ -37,11 +37,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // If no login, it will redirect to /login page.
 //        http.authorizeRequests().antMatchers("/welcome", "/productList", "/shoppingCartConfirmation", 
 //        		"/shoppingCart", "/buyProduct", "/shoppingCartCustomer", "/shoppingCartAnswers").access("hasRole('ROLE_SHOPPING_CART_USER')");
+//        http.authorizeRequests().antMatchers("/userInfo").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
         
         http.authorizeRequests().antMatchers("/shopping-cart/*").access("hasRole('ROLE_SHOPPING_CART_USER')");
-        
-        
-        
         
         // When the user has logged in as XX.
         // But access a page that requires role YY,
