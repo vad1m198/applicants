0df6b25e171fc2b971fdee11697b92cd83f2339d
Vad1m198 <Persidskij_on_ferrari@ukr.net>
2017-04-10 12:30:22 +0300

    fix product service @Service was added

--- src/main/java/org/dynamo/service/ProductServiceImpl.java
+++ src/main/java/org/dynamo/service/ProductServiceImpl.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.dynamo.dao.ProductDao;
 import org.dynamo.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProductServiceImpl implements ProductService {
 	
 	@Autowired
